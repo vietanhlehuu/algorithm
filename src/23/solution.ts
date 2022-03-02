e942a63007c34fb5bcdcc7441935349557835a41
vietanhlehuu <lehuuvietanh@gmail.com>
2022-03-02 15:05:05 +0700

    add complexity for question 23

--- src/23/solution.ts
+++ src/23/solution.ts
@@ -1,14 +1,20 @@
+// Time: O(s.length x words.length x words[0].length)
+// Space: O(s.length + words.length)
+
 export function findSubstring(s: string, words: string[]): number[] {
   if (words.length == 0) {
     return [];
   }
+  // worst case, store all index -> O(s.length)
   const result: number[] = [];
   const wordLength = words[0].length;
   const wordsCount = words.length;
   const totalWordsLength = wordsCount * wordLength;
 
+  // O(wordCount)
   const wordsMap = new Map<string, number>();
 
+  // O(words.length)
   for (let i = 0; i < words.length; i++) {
     const word = words[i];
     if (!wordsMap.has(word)) {
@@ -17,10 +23,12 @@ export function findSubstring(s: string, words: string[]): number[] {
     wordsMap.set(word, wordsMap.get(word)! + 1);
   }
 
+  // O(s.length)
   for (let i = 0; i < s.length - totalWordsLength + 1; i++) {
     const wordSeen = new Map<string, number>();
-
+    // O (wordCount)
     for (let j = i; j < i + totalWordsLength; j += wordLength) {
+      // O(wordLength)
       const word = s.slice(j, j + wordLength);
 
       if (wordsMap.has(word)) {
